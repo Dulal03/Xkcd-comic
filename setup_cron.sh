33583298accc3f64b2ae97f9df1be69b8f1461cc
Dulal Ghosh <113444487+Dulal03@users.noreply.github.com>
2025-07-06 01:43:35 +0530

    Update setup_cron.sh

--- setup_cron.sh
+++ setup_cron.sh
@@ -2,4 +2,4 @@
 # This script should set up a CRON job to run cron.php every 24 hours.
 # You need to implement the CRON setup logic here.
 crontab -e 
-0 10 * * * C:\xampp\htdocs\Rt_campus\xkcd-Dulal03\src\cron.php;
+0 10 * * * "use the cron.php's path";
